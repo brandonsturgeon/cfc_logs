0d33c9e3ad1f6bad66e778d72e207173d2799f37
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-26 13:05:01 -0500

    Adds much of the data parsing

--- parse.py
+++ parse.py
@@ -1,4 +1,6 @@
 import re
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 
 class LogLine():
@@ -27,10 +29,10 @@ class LogLine():
         return self.line_type == "kill"
 
     def _determine_line_type(self):
-        ent_spawned_regex = r'\[\d\d:\d\d:\d\d] (.*)<(.*)> spawned (?:vehicle|model|sent) (.*)'
-        chat_regex = r'^\[\d\d:\d\d:\d\d] (.*): (.*)\n'
-        tool_used_regex = r'^\[\d\d:\d\d:\d\d] (.*)<(.*)> used the tool (\w*) on (.*)\n'
-        kill_regex = r'^\[\d\d:\d\d:\d\d] (.*) killed (.*) using (.*)\n'
+        ent_spawned_regex = r'\[\d\d:\d\d:\d\d] (.*)<(.*)> spawned (?:vehicle|model|sent) (.*)\r'
+        chat_regex = r'\[\d\d:\d\d:\d\d] (.*): (.*)\r'
+        tool_used_regex = r'\[\d\d:\d\d:\d\d] (.*)<(.*)> used the tool (\w*) on (.*)\r'
+        kill_regex = r'\[\d\d:\d\d:\d\d] (.*) killed (.*) using (.*)\r'
 
         matches = []
 
@@ -52,17 +54,17 @@ class LogLine():
             matches.append({'match': kill, 'line_type': 'kill'})
 
         if len(matches) > 1:
-            print()
+            print('')
             print('MULTIPLE MATCHES FOUND, CHOOSING FIRST')
             print(matches)
             print(self.text)
-            print()
+            print('')
 
         if len(matches) == 0:
-            print()
-            print('NO MATCHES FOUND, RETURNING NONE')
-            print(self.text)
-            print()
+            #print('')
+            #print('NO MATCHES FOUND, RETURNING NONE')
+            #print(self.text)
+            #print('')
             return None
 
         match = matches[0]
@@ -80,8 +82,15 @@ class LogLine():
 
         if line_type == 'ent_spawned':
             return match.group(3)
+        elif line_type == 'tool_used':
+            return match.group(3)
+        elif line_type == 'kill':
+            return match.group(2)
+            # return {'victim': match.group(2),
+            #         'weapon': match.group(3)}
 
-        elif line_type == 
+        return None
+
 
 class Parser():
     def __init__(self, filename):
@@ -92,6 +101,11 @@ class Parser():
 
         self.tools_used_global = {}
         self.tools_used_people = {}
+
+        self.kills_global = {}
+        self.kills_people = {}
+        self.deaths_global = {}
+        self.deaths_people = {}
 
         self.ents_spawned_global = {}
         self.ents_spawned_people = {}
@@ -107,9 +121,66 @@ class Parser():
             for line in lines:
                 self.parse_line(line)
 
-    @staticmethod
-    def parse_line(line_text):
+            pp.pprint(self.deaths_people)
+
+    def parse_line(self, line_text):
         line = LogLine(line_text)
+
+        line_type = line.line_type
+
+        if line_type:
+            player = line.player
+            subject = line.subject
+
+            if line_type == 'ent_spawned':
+                self.add_spawned_ent(player, subject)
+
+            elif line_type == 'tool_used':
+                self.add_tool_used(player, subject)
+
+            elif line_type == 'kill':
+                self.add_kill(player, subject)
+                self.add_death(subject, player)
+
+            elif line_type == 'chat':
+                self.add_words(player, subject)
+
+    def add_spawned_ent(self, player, ent):
+        global_data = self.ents_spawned_global
+        player_data = self.ents_spawned_people.get(player, {})
+
+        global_data[ent] = global_data.get(ent, 0) + 1
+
+        player_data[ent] = player_data.get(ent, 0) + 1
+        self.ents_spawned_people[player] = player_data
+
+    def add_tool_used(self, player, tool):
+        global_data = self.tools_used_global
+        player_data = self.tools_used_people.get(player, {})
+
+        global_data[tool] = global_data.get(tool, 0) + 1
+
+        player_data[tool] = player_data.get(tool, 0) + 1
+        self.tools_used_people[player] = player_data
+
+    def add_kill(self, player, victim):
+        global_data = self.kills_global
+        player_data = self.kills_people.get(player, {})
+
+        global_data[player] = global_data.get(player, 0) + 1
+
+        player_data[victim] = player_data.get(victim, 0) + 1
+        self.kills_people[player] = player_data
+
+    def add_death(self, player, killer):
+        global_data = self.deaths_global
+        player_data = self.deaths_people.get(player, {})
+
+        global_data[player] = global_data.get(player, 0) + 1
+
+        player_data[killer] = player_data.get(killer, 0) + 1
+        self.deaths_people[player] = player_data
+
 
 if __name__ == '__main__':
     Parser('ulx_logs/01-18-16.txt')
