3d55e887d283ecf21afca5baf142784d9c4d2c0a
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-26 15:32:08 -0500

    Primes the script to actually write data

--- parse.py
+++ parse.py
@@ -1,5 +1,9 @@
 import re
 import pprint
+from os import listdir
+import json
+import time
+
 pp = pprint.PrettyPrinter(indent=4)
 
 
@@ -16,23 +20,27 @@ class LogLine():
 
         self.regex_match = None
 
+    @property
     def is_ent_spawned_line(self):
         return self.line_type == "ent_spawned"
-
+    
+    @property
     def is_tool_used_line(self):
         return self.line_type == "tool_used"
 
+    @property
     def is_chat_line(self):
         return self.line_type == "chat"
 
+    @property
     def is_kill_line(self):
         return self.line_type == "kill"
 
     def _determine_line_type(self):
-        ent_spawned_regex = r'\[\d\d:\d\d:\d\d] (.*)<(.*)> spawned (?:vehicle|model|sent) (.*)\r'
-        chat_regex = r'\[\d\d:\d\d:\d\d] (.*): (.*)\r'
-        tool_used_regex = r'\[\d\d:\d\d:\d\d] (.*)<(.*)> used the tool (\w*) on (.*)\r'
-        kill_regex = r'\[\d\d:\d\d:\d\d] (.*) killed (.*) using (.*)\r'
+        ent_spawned_regex = r'^\[\d\d:\d\d:\d\d] (.*)<(.*)> spawned (?:vehicle|model|sent) (.*)\r'
+        chat_regex = r'^\[\d\d:\d\d:\d\d] (.*): (.*)\r'
+        tool_used_regex = r'^\[\d\d:\d\d:\d\d] (.*)<(.*)> used the tool (\w*) on (.*)\r'
+        kill_regex = r'^\[\d\d:\d\d:\d\d] (.*) killed (.*) using (.*)\r'
 
         matches = []
 
@@ -56,8 +64,6 @@ class LogLine():
         if len(matches) > 1:
             print('')
             print('MULTIPLE MATCHES FOUND, CHOOSING FIRST')
-            print(matches)
-            print(self.text)
             print('')
 
         if len(matches) == 0:
@@ -72,7 +78,6 @@ class LogLine():
 
         return match['line_type']
 
-
     def _get_player(self):
         return self.regex_match.group(1)
 
@@ -80,11 +85,13 @@ class LogLine():
         line_type = self.line_type
         match = self.regex_match
 
-        if line_type == 'ent_spawned':
+        if self.is_ent_spawned_line:
             return match.group(3)
-        elif line_type == 'tool_used':
+
+        elif self.is_tool_used_line:
             return match.group(3)
-        elif line_type == 'kill':
+
+        elif self.is_kill_line:
             return match.group(2)
             # return {'victim': match.group(2),
             #         'weapon': match.group(3)}
@@ -93,8 +100,8 @@ class LogLine():
 
 
 class Parser():
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, file_paths):
+        self.file_paths = file_paths
 
         self.words_said_global = {}
         self.words_said_people = {}
@@ -113,36 +120,62 @@ class Parser():
         self.parse_file()
 
     def parse_file(self):
-        with open(self.filename) as log:
-            log_text = log.read()
-            lines = log_text.split("\n")
+        for data_file in self.file_paths:
+            filename = "ulx_logs/"+data_file
+            
+            with open(filename) as log:
+                log_text = log.read()
+                lines = log_text.split("\n")
 
-            # Main parsing loop
-            for line in lines:
-                self.parse_line(line)
+                # Main parsing loop
+                for line in lines:
+                    self.parse_line(line)
 
-            pp.pprint(self.deaths_people)
+        timestamp = int(time.time())
+        json_deaths = json.dumps({'global_deaths': self.deaths_global, 'player_deaths': self.deaths_people})
+        json_kills = json.dumps({'global_kills': self.kills_global, 'player_kills': self.kills_people})
+        json_tools = json.dumps({'global_tools_used': self.tools_used_global, 'player_tools_used': self.tools_used_people})
+        json_ents = json.dumps({'global_ents_spawned': self.ents_spawned_global, 'player_ents_spawned': self.ents_spawned_people})
+        with open('json_deaths', 'w') as save_file:
+            print "Saving json_deaths.."
+            save_file.write(json_deaths)
+            print "Saved!"
+
+        with open('json_kills', 'w') as save_file:
+            print "Saving json_kills.."
+            save_file.write(json_kills)
+            print "Saved!"
+
+        with open('json_tools', 'w') as save_file:
+            print "Saving json_tools.."
+            save_file.write(json_tools)
+            print "Saved!"
+
+        with open('json_ents', 'w') as save_file:
+            print "Saving json_ents.."
+            save_file.write(json_ents)
+            print "Saved!"
+
+
 
     def parse_line(self, line_text):
         line = LogLine(line_text)
 
-        line_type = line.line_type
-
-        if line_type:
+        if line.line_type:
             player = line.player
             subject = line.subject
 
-            if line_type == 'ent_spawned':
+            if line.is_ent_spawned_line:
                 self.add_spawned_ent(player, subject)
 
-            elif line_type == 'tool_used':
+            if line.is_tool_used_line:
                 self.add_tool_used(player, subject)
 
-            elif line_type == 'kill':
+            elif line.is_kill_line:
                 self.add_kill(player, subject)
                 self.add_death(subject, player)
 
-            elif line_type == 'chat':
+            elif line.is_chat_line:
                 self.add_words(player, subject)
 
     def add_spawned_ent(self, player, ent):
@@ -181,6 +214,9 @@ class Parser():
         player_data[killer] = player_data.get(killer, 0) + 1
         self.deaths_people[player] = player_data
 
+    def add_words(self, player, text):
+        pass
 
 if __name__ == '__main__':
-    Parser('ulx_logs/01-18-16.txt')
+    files = listdir('ulx_logs/')
+    Parser(files)
