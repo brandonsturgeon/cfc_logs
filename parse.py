c9323b9aab4c9949991e75c6c703bfeaba72774f
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-10-26 10:04:48 -0500

    Adds some more logic. Rework is needed.

--- parse.py
+++ parse.py
@@ -4,8 +4,15 @@ import re
 class LogLine():
     def __init__(self, line_text):
         self.text = line_text
+        self.player = None
+        self.subject = None
+
         self.line_type = self._determine_line_type()
-        self.player = self._get_player()
+        if self.line_type:
+            self.player = self._get_player()
+            self.subject = self._get_subject()
+
+        self.regex_match = None
 
     def is_ent_spawned_line(self):
         return self.line_type == "ent_spawned"
@@ -20,16 +27,65 @@ class LogLine():
         return self.line_type == "kill"
 
     def _determine_line_type(self):
-        pass
+        ent_spawned_regex = r'\[\d\d:\d\d:\d\d] (.*)<(.*)> spawned (?:vehicle|model|sent) (.*)'
+        chat_regex = r'^\[\d\d:\d\d:\d\d] (.*): (.*)\n'
+        tool_used_regex = r'^\[\d\d:\d\d:\d\d] (.*)<(.*)> used the tool (\w*) on (.*)\n'
+        kill_regex = r'^\[\d\d:\d\d:\d\d] (.*) killed (.*) using (.*)\n'
+
+        matches = []
+
+        # TODO: reorder these in order of most frequently found lines
+        ent_spawned = re.search(ent_spawned_regex, self.text)
+        if ent_spawned:
+            matches.append({'match': ent_spawned, 'line_type': 'ent_spawned'})
+
+        tool_used = re.search(tool_used_regex, self.text)
+        if tool_used:
+            matches.append({'match': tool_used, 'line_type': 'tool_used'})
+
+        chat = re.search(chat_regex, self.text)
+        if chat:
+            matches.append({'match': chat, 'line_type': 'chat'})
+
+        kill = re.search(kill_regex, self.text)
+        if kill:
+            matches.append({'match': kill, 'line_type': 'kill'})
+
+        if len(matches) > 1:
+            print()
+            print('MULTIPLE MATCHES FOUND, CHOOSING FIRST')
+            print(matches)
+            print(self.text)
+            print()
+
+        if len(matches) == 0:
+            print()
+            print('NO MATCHES FOUND, RETURNING NONE')
+            print(self.text)
+            print()
+            return None
+
+        match = matches[0]
+        self.regex_match = match['match']
+
+        return match['line_type']
+
 
     def _get_player(self):
-        pass
+        return self.regex_match.group(1)
 
+    def _get_subject(self):
+        line_type = self.line_type
+        match = self.regex_match
+
+        if line_type == 'ent_spawned':
+            return match.group(3)
+
+        elif line_type == 
 
 class Parser():
     def __init__(self, filename):
         self.filename = filename
-        self.parse_file()
 
         self.words_said_global = {}
         self.words_said_people = {}
@@ -40,6 +96,8 @@ class Parser():
         self.ents_spawned_global = {}
         self.ents_spawned_people = {}
 
+        self.parse_file()
+
     def parse_file(self):
         with open(self.filename) as log:
             log_text = log.read()
@@ -49,9 +107,9 @@ class Parser():
             for line in lines:
                 self.parse_line(line)
 
+    @staticmethod
     def parse_line(line_text):
         line = LogLine(line_text)
-        print(line)
 
 if __name__ == '__main__':
     Parser('ulx_logs/01-18-16.txt')
